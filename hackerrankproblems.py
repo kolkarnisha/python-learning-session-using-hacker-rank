8d1cac12f5f6f176558a3b16f64e0bc4d33bb9fa
kolkarnisha <neeshakolkar@gmail.com>
2026-06-21 12:37:16 +0530

    Add hackerrankproblems.py file

---
+++ hackerrankproblems.py
@@ -0,0 +1,34 @@
+def is_palindrome(j):
+    # Remove spaces and convert to lowercase for a fair comparison
+    cleaned = ''.join(j.split()).lower()
+    # Check if the cleaned string is the same forwards and backwards
+    return cleaned == cleaned[::-1]
+
+# Let's test it out!
+print(is_palindrome("radar"))  # This should return True
+print(is_palindrome("hello"))  # This should return False
+print("")
+def reverse_string(text):
+    # Remove spaces and convert to lowercase if you want
+    cleaned = ''.join(text.split()).lower()
+    # Reverse it using slicing
+    return cleaned[::-1]
+
+# Take input from user
+user_input = input("Enter a string: ")
+print(user_input)
+reversed_text = reverse_string(user_input)
+print("Reversed string:", reversed_text)
+print("this was a play with strings its an amazing concept suported by python")
+print("""The provided code stub reads two integers,  and , from STDIN.
+Add logic to print two lines. The first line should contain the result of integer division,  // . 
+The second line should contain the result of float division,  / 
+""")
+if __name__ == '__main__':
+    a = int(input("enter your first number: "))
+    b = int(input("enter your second number: "))
+    print(a//b)
+    print(a/b)
+
+
+            
